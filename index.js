a3a8448ea403da383625411a4f212ec4f5f2b773
Botir <t19a003b@jdu.uz>
2023-06-10 07:18:34 +0500

    HTML bilan ishlash

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
-const server = http.createServer((request, response) => {
-  console.log(request.url);
+const server = http.createServer((req, res) => {
+  if(req.method === 'GET'){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    if(req.url === '/'){
+      fs.readFile(path.join(__dirname, 'templates', 'index.html'), 'utf-8', (err, content) =>{
+        if(err) throw err
+        res.end(content)
+      })
+    } else if(req.url === '/about'){
+      fs.readFile(path.join(__dirname, 'templates', 'about.html'), 'utf-8', (err, content) =>{
+        if(err) throw err
+        res.end(content)
+      })
+    } else if(req.url === '/contact'){
+      fs.readFile(path.join(__dirname, 'templates', 'contact.html'), 'utf-8', (err, content) =>{
+        if(err) throw err
+        res.end(content)
+      })
+    }
+  } else if(req.method === 'POST'){
+    const email = []
 
-  response.write('<h1>Hello world!</h1>')
-  response.write('<h1>Hello world!</h1>')
-  response.end()
+    req.on('data', data => {
+      email.push(Buffer.from(data))
+    })
+
+    req.on('end', () => {
+      const message = email.toString().split('=')[1]
+
+      res.end(`Email succesfully  added: ${message}`)
+    })
+
+  }
+
 })
 
 server.listen(3000, () =>{
